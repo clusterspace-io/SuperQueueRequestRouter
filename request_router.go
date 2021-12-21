1dec64e6c949194f4455543f969f94d993f81266
Dan Goodman <danthegoodman@icloud.com>
2021-12-21 11:28:56 -0500

    ignore draining partitions

--- request_router.go
+++ request_router.go
@@ -9,13 +9,23 @@ type NackRecordRequest struct {
 	DelayMS *float64 `json:"delay_ms"`
 }
 
-// Gets a random partition from a queue
-func GetRandomPartition(p *PartitionStoredRecord) PartitionSDRecord {
+// Gets a random partition from a queue, ignores draining partitions
+func GetRandomPartition(p *PartitionStoredRecord) *PartitionSDRecord {
+	var nonDrainingPartitions []PartitionSDRecord
+	for _, i := range *p {
+		if !i.IsDraining {
+			nonDrainingPartitions = append(nonDrainingPartitions)
+		}
+	}
+	if len(nonDrainingPartitions) == 0 {
+		return nil
+	}
 	rand.Seed(time.Now().UnixNano())
-	index := rand.Intn(len(*p))
-	return (*p)[index]
+	index := rand.Intn(len(nonDrainingPartitions))
+	return &nonDrainingPartitions[index]
 }
 
+// Gets a specific partition of a queue, includes draining partitions
 func GetPartition(p *PartitionStoredRecord, partitionID string) *PartitionSDRecord {
 	for _, i := range *p {
 		if i.Partition == partitionID {
