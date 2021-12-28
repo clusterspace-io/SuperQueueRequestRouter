a560bd539490b5bdcc847bdf670b30b2c6798f2e
Dan Goodman <danthegoodman@icloud.com>
2021-12-28 15:40:39 -0500

    protocol in logs

--- http_server.go
+++ http_server.go
@@ -56,7 +56,16 @@ func StartHTTPServer() {
 	}
 	Server.Echo.HideBanner = true
 	Server.Echo.Use(MetricsHandler)
-	Server.Echo.Use(middleware.Logger())
+	// Server.Echo.Use(middleware.Logger())
+	config := middleware.LoggerConfig{
+		Skipper: middleware.DefaultSkipper,
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"` +
+			`,"bytes_in":${bytes_in},"bytes_out":${bytes_out},"proto":"${protocol}"}` + "\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+	}
+	Server.Echo.Use(middleware.LoggerWithConfig(config))
 	Server.Echo.Validator = &CustomValidator{validator: validator.New()}
 	t := &http2.Transport{
 		AllowHTTP: true,
